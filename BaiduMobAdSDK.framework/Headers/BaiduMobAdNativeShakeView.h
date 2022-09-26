d06875625ec9759395a702ef5798c5e220b788b3
baidumobsdk <baidumobsdk@163.com>
2022-09-26 10:08:25 +0800

    release 4.901

---
+++ BaiduMobAdSDK.framework/Headers/BaiduMobAdNativeShakeView.h
@@ -0,0 +1,47 @@
+//
+//  BaiduMobAdNativeShakeView.h
+//  BaiduMobAdSDK
+//
+//  Created by sunmingzhe01 on 2022/9/6.
+//  Copyright © 2022 Baidu Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class BaiduMobAdNativeAdObject;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol BaiduMobAdNativeShakeViewDelegate <NSObject>
+
+// 摇一摇销毁
+- (void)nativeShakeViewDismiss:(UIView *)shakeView adObject:(BaiduMobAdNativeAdObject *)object;
+
+@end
+
+@interface BaiduMobAdNativeShakeView : UIView
+
+@property (nonatomic, weak) id <BaiduMobAdNativeShakeViewDelegate> delegate;
+
+/**
+ * 初始化摇一摇组件，组件高度需要大于80pt。
+ * 摇一摇文案内容为：摇动手机  了解更多  如果组件宽度不够，则会自动将宽度适配文字长度。
+ * @param fram 组件frame
+ * @param object BaiduMobAdNativeAdObject对象
+ * @return 组件实例对象
+ * 如果返回为nil，原因1：组件尺寸不合规；原因2：广告位不支持
+ */
+- (instancetype)initWithFrame:(CGRect)frame adObject:(BaiduMobAdNativeAdObject *)object;
+
+// 开启摇动检测
+- (void)startShake;
+
+// 停止摇动检测
+- (void)stopShake;
+
+// 销毁摇一摇组件
+- (void)destoryShakeView;
+
+@end
+
+
+NS_ASSUME_NONNULL_END
--- BaiduMobAdSDK.podspec
+++ BaiduMobAdSDK.podspec
@@ -16,9 +16,9 @@ Pod::Spec.new do |s|
   #
 
   s.name         = 'BaiduMobAdSDK'
-  s.version      = '4.9'
+  s.version      = '4.901'
   s.summary      = "mobile ad sdk for ios"
-  s.source       = { :git => "https://github.com/baidumobsdk/iosmobsdk.git", :tag => "4.9" }
+  s.source       = { :git => "https://github.com/baidumobsdk/iosmobsdk.git", :tag => "4.901" }
 
 
   # This description is used to generate tags and improve search results.
