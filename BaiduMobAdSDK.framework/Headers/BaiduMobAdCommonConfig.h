bcf8a8444c7d3581ee9096bf51553685245f31ba
chenyaohuang <chenyaohuang@baidu.com>
2023-08-12 17:36:57 +0800

    release 5.313

--- BaiduMobAdSDK.framework/Headers/BaiduMobAdCommonConfig.h
+++ BaiduMobAdSDK.framework/Headers/BaiduMobAdCommonConfig.h
@@ -9,7 +9,7 @@
 #ifndef BaiduMobAdSdk_BaiduMobAdCommonConfig_h
 #define BaiduMobAdSdk_BaiduMobAdCommonConfig_h
 // SDK版本号
-#define SDK_VERSION_IN_MSSP @"5.311"
+#define SDK_VERSION_IN_MSSP @"5.313"
 
 typedef void (^BaiduMobAdViewCompletionBlock)(NSArray *errors);
 
